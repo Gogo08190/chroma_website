fb11df8c67b48b742aa0862abcc06244dac5c15c
Gogo08 <gogo08190@gmail.com>
2022-01-28 21:13:59 +0100

    Footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,9 @@
 <footer id="page-footer">
-
+  <div id="copyright">
+    <div class="uk-container">
+      <div class="uk-text-center">
+        <p>© <?php echo date ('Y'); ?> – Développé par <a href="mailto:contact@gogo08.fr" rel="nofollow" title="Gogo08" target="_blank">Gogo08</a>. Tous droits réservés. <a href="/mentions-legales" rel="nofollow">Mentions légales.</a></p>
+      </div>
+    </div>
+  </div>
 </footer>
